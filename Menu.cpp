3203cf648c1e0bf431518c4bb32f32b3dda4fa40
jnuno <up201505648@fe.up.pt>
2016-11-01 15:10:54 +0000

    .

--- Menu.cpp
+++ Menu.cpp
@@ -1,7 +1,9 @@
-/*#include "Menu.h"
+
+#include "Menu.h"
 
 //TODO All Menus
 //TODO Do we create new class for every University or we do for only 1 University??
+/*
 unsigned int menuOption(string badInput, string normalInput, vector<string> tryAgain, unsigned int min, unsigned int max)
 {
 	unsigned int option;
@@ -16,4 +18,5 @@ unsigned int menuOption(string badInput, string normalInput, vector<string> tryA
 		else break;
 	} while (true);
 	return option;
-}*/
+}
+*/
--- Menu.h
+++ Menu.h
@@ -1,7 +1,12 @@
-/*#pragma once
+#pragma once
+#ifndef MENU_H
+#define MENU_H
+
 
 #include "University.h"
 #include "Student.h"
 #include "Utilities.h"
 
-unsigned int menuOption(string badInput, string normalInput, vector<string> tryAgain, unsigned int min, unsigned int max);*/
+/*unsigned int menuOption(string badInput, string normalInput, vector<string> tryAgain, unsigned int min, unsigned int max);*/
+
+#endif
